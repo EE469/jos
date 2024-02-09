859a67cefe914d3fb503aad8601e8a02c1a13434
Sudhanva Donakonda <sdonakon@eceprog2.ecn.purdue.edu>
2024-02-09 17:08:18 -0500

    Working but takes long

--- kern/pmap.c
+++ kern/pmap.c
@@ -184,7 +184,7 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
-	boot_map_region(kern_pgdir,UPAGES,PGSIZE,PADDR(pages),PTE_U | PTE_P);
+	boot_map_region(kern_pgdir,UPAGES,ROUNDUP(npages * sizeof(struct PageInfo), PGSIZE),PADDR(pages),PTE_U | PTE_P);
 	
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
@@ -206,7 +206,7 @@ mem_init(void)
 	// we just set up the mapping aKERNBASnyway.
 	// Permissions: kernel RW, user NONE
 	// Your code goes here:
-	boot_map_region(kern_pgdir,KERNBASE,ROUNDUP(npages * PGSIZE, PGSIZE),0,PTE_W | PTE_P);
+	boot_map_region(kern_pgdir,KERNBASE,-KERNBASE,0,PTE_W | PTE_P);
 	// Check that the initial page directory has been set up correctly.
 	check_kern_pgdir();
 
@@ -419,8 +419,8 @@ boot_map_region(pde_t *pgdir, uintptr_t va, size_t size, physaddr_t pa, int perm
 {
 	// Fill this function in
 	for (int i =0; i< size; i++) {
-		pte_t * pte = pgdir_walk(pgdir,(const void *  )(va + i * PGSIZE),1);
-		*pte = PTE_ADDR(pa + i * PGSIZE) | perm | PTE_P;
+		pte_t * pte = pgdir_walk(pgdir,(const void *  )(va + i),1);
+		*pte = PTE_ADDR(pa + i) | perm | PTE_P;
 
 	} 
 }
