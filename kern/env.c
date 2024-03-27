137955ddb09c8cdcaee0dd97fb84b173c9635bf9
JonHuang1 <jonhuang918@gmail.com>
2024-03-27 10:35:03 -0400

    5 filled out, can't test atm

--- kern/env.c
+++ kern/env.c
@@ -555,6 +555,7 @@ env_run(struct Env *e)
 	curenv->env_runs++;
 	curenv->env_status = ENV_RUNNING;
 	lcr3(PADDR(curenv->env_pgdir));
+	unlock_kernel();
 	env_pop_tf(&(curenv->env_tf));
 
 	// panic("env_run not yet implemented");
