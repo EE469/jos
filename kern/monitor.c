8f1ccec002158a8f1fe36a597798e0dabf3a7b23
Aarav Sumit Patel <pate1830@eceprog5.ecn.purdue.edu>
2025-03-05 23:24:53 -0500

    changes to lab2 after handin

--- kern/monitor.c
+++ kern/monitor.c
@@ -14,6 +14,7 @@
 #include <kern/hidden.h>
 
 #define CMDBUF_SIZE	80	// enough for one VGA text line
+int exec_hidden_cases(int argc, char **argv, struct Trapframe *tf);
 
 int
 test(int argc, char **argv, struct Trapframe *tf)
